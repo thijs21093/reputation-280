dc6abff5bae61dc3347722c35d1bf81ca8d3d1a3
thijs21093 <thijsdeboer@outlook.com>
2022-03-11 10:49:05 +0100

    Added plots and additional code

--- sentiment/sentiment.R
+++ sentiment/sentiment.R
@@ -6,6 +6,9 @@ library(googleLanguageR)
 gl_auth(GL_AUTH)
 
 library(dplyr)
+library(ggplot2)
+library(scales)
+library(tidyr)
 
 #load documents
 load("media/media.Rdata")
@@ -15,7 +18,6 @@ load("to_tweets_2")
 #only keep english tweets
 tibble.from <- tibble.from[tibble.from$lang=="en",]
 tibble.to <- tibble.to[tibble.to$lang=="en",]
-
 
 # combine headings and text in media df to retrieve as many references to the agencies as possible
 media$complete_text <- paste(paste0(media$heading, "."), media$text)
@@ -46,19 +48,31 @@ for (i in 1:length(trial_results$entities)){
 
 trial_dataframe <-bind_rows(trial_results$entities)
 trial_dataframe$name <- tolower(trial_dataframe$name)
+trial_dataframe <- trial_dataframe %>%
+  drop_na(score)
 
 # only keep matches with the agency
 agency_descriptives <- c("frontex", "border control agency", "border patrol agency", "coast guard agency", "border agency")
 trial_dataframe <- trial_dataframe[trial_dataframe$name %in% agency_descriptives,]
 
+# introduce combined measure of magnitude and score to avoid misclassifications
+trial_dataframe$com_score <- trial_dataframe$magnitude*trial_dataframe$score
+
+ggplot(trial_dataframe, aes(x = as.factor(com_score))) +  
+  geom_bar(aes(y = (..count..)/sum(..count..))) + 
+  scale_y_continuous(labels = percent, n.breaks = 10)
+
 # best approach seems to be to measure positivity and negativity separately
-# Let's take the metion with the lowest score to see differences.
+# Let's take the mention with the lowest score to see differences.
 trial_dataframe <- trial_dataframe %>% 
   group_by(id) %>% 
-  slice(which.min(score))
+  summarise(com_score_mean = mean(com_score),
+            com_score_min = min(com_score),
+            com_score_max = max(com_score),
+            hits = n())
 
-#introduce combined measure of magnitude and score to avoid misclassifications
-trial_dataframe$com_score <- trial_dataframe$magnitude*trial_dataframe$score
+ggplot(trial_dataframe, aes(x = com_score_mean)) +  
+  geom_histogram(colour="black", fill="white")
 
 #join text and score dataset
 media_sentiment <- left_join(media_trial, trial_dataframe, by="id")
